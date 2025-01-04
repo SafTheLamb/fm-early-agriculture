b51ba8a5989c42a8eb5f6b98f48e7ba1d2847ad8
sapphiriza <sapphiriza@gmail.com>
2025-01-04 11:34:07 -0800

    Version 0.9.4: Diversitree compat.

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,7 +1,11 @@
 -- Fix trees not being plantable on Alien Biomes tiles
 -- Credit to blooest's fix for this bug report on the Alien Biomes discord!
 -- I updated it slightly so that it shouldn't break if AB or another mod fixes this themselves
-local tree_plant = data.raw["plant"]["tree-plant"]
+local tree_plant = data.raw.plant["tree-plant"]
+if mods["Diversitree"] then
+  tree_plant = data.raw.plant["s6xdvt-fake-tree"]
+end
+
 if mods["alien-biomes"] and tree_plant then
   -- convert list of tiles into a dictionary for quick reference
   local ab_fertiles = alien_biomes.list_tiles(alien_biomes.require_tag(alien_biomes.all_tiles(), {"dirt", "grass"}))
